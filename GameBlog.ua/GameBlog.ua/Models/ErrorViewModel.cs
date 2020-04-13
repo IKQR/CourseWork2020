e0cda01929f4e1ae2a4531d6a4885bb75f139745
Ilya Kusik <kusikilya2001@outlook.com>
2020-04-13 15:45:12 +0300

    JustTemporaryFile

---
+++ GameBlog.ua/GameBlog.ua/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace GameBlog.ua.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
