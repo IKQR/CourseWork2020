12e09aa51bf81c26cd4ce800f69801d98d166cd2
Ilya Kusik <kusikilya2001@outlook.com>
2020-04-22 15:37:25 +0300

    index on FastChanges: 1dd24d5 New breath

---
+++ GameBlog/GameBlog.Initializer/DbInitializer.cs
@@ -0,0 +1,29 @@
+﻿using GameBlog.DAL.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Threading.Tasks;
+
+namespace GameBlog.Initializer
+{
+    public class DbInitializer
+    {
+        public static async Task InitializeAsync(IServiceProvider services)
+        {
+            var context = services.GetRequiredService<GameBlogDbContext>();
+            var userManager = services.GetRequiredService<UserManager<User>>();
+            var rolesManager = services.GetRequiredService<RoleManager<Role>>();
+
+            context.Database.EnsureDeleted();
+            context.Database.EnsureCreated();
+
+            await ImageInitializer.InitializeAsync(context);
+
+            await RoleInitializer.InitializeAsync(userManager, rolesManager);
+
+        }
+    }
+}
+
---
+++ GameBlog/Initializers/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Initializers
+{
+    public class Class1
+    {
+    }
+}
